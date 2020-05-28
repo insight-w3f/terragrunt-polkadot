47a3e0708d7cfbbb1d795a06da9e86de4949f3fb
rob <robcannonxyz@gmail.com>
2020-05-27 23:15:40 -0700

    updating tests with new tg module

--- polkadot/api/aws/test/aws_test.go
+++ polkadot/api/aws/test/aws_test.go
@@ -4,7 +4,6 @@ import (
 	"github.com/gruntwork-io/terratest/modules/files"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	test_structure "github.com/gruntwork-io/terratest/modules/test-structure"
-	"github.com/insight-w3f/terragrunt-polkadot/utils"
 	"log"
 	"path"
 	"testing"
@@ -13,7 +12,7 @@ import (
 func TestTerragruntAwsComplete(t *testing.T) {
 	t.Parallel()
 
-	folderPath := "../../../"
+	folderPath := ".."
 	baseFolder, err := files.CopyTerragruntFolderToTemp(folderPath, t.Name())
 	if err != nil {
 		log.Println(err)
@@ -21,23 +20,17 @@ func TestTerragruntAwsComplete(t *testing.T) {
 
 	log.Print(baseFolder)
 
-	fixturesDir := path.Join(folderPath, "test/fixtures")
-	privateKeyPath := path.Join(fixturesDir, "./keys/id_rsa_test")
-	publicKeyPath := path.Join(fixturesDir, "./keys/id_rsa_test.pub")
-	utils.GenerateKeys(privateKeyPath, publicKeyPath)
-
-	testFolder := "polkadot/api/aws"
 
 	terraformOptions := &terraform.Options{
-		TerraformDir:    path.Join(baseFolder, testFolder),
+		TerraformDir:    path.Join(baseFolder),
 		TerraformBinary: "terragrunt",
 	}
 
 	defer test_structure.RunTestStage(t, "teardown", func() {
-		utils.TgDestroyAll(t, terraformOptions, path.Join(baseFolder, testFolder))
+		terraform.TgDestroyAll(t, terraformOptions)
 	})
 
 	test_structure.RunTestStage(t, "setup", func() {
-		utils.TgApplyAll(t, terraformOptions, path.Join(baseFolder, testFolder))
+		terraform.TgApplyAll(t, terraformOptions)
 	})
 }
